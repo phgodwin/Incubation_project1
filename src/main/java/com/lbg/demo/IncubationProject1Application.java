32ac5c2277c3beba8e2d4ee2b3d24bb441fcddf6
phgodwin <paige227@hotmail.co.uk>
2024-02-26 13:57:34 +0000

    first commit - set up

---
+++ src/main/java/com/lbg/demo/IncubationProject1Application.java
@@ -0,0 +1,13 @@
+package com.lbg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubationProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubationProject1Application.class, args);
+	}
+
+}
